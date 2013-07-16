b8c26d85788cc2dac7e6948828725855e5bc6293
Jakub Matejka <jakub@keboola.com>
2013-07-16 11:16:44 +0200

    Better handling of GD errors

---
+++ Exception/ClientException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Jakub Matejka <jakub@keboola.com>
+ * @date 2013-07-16
+ */
+
+namespace Keboola\GoodDataWriter\Exception;
+
+use Syrup\ComponentBundle\Exception\SyrupComponentException;
+
+class ClientException extends SyrupComponentException
+{
+	public function __construct($message)
+	{
+		parent::__construct(400, $message);
+	}
+}
