cb045ac2129d682f89e010b484310f13a23b0f11
Jakub Matejka <jakub@keboola.com>
2013-09-25 09:09:58 +0200

    Improved s3 logging

--- Job/UploadTable.php
+++ Job/UploadTable.php
@@ -257,7 +257,7 @@ class UploadTable extends GenericJob
 		if ($error) {
 			$result['error'] = $error;
 		}
-		return $this->_prepareResult($job['id'], $result, $output);
+		return $this->_prepareResult($job['id'], $result, $output, $tmpFolderName);
 	}
 
 
