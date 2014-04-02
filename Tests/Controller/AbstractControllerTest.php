3600f069d3cf0fb899dc5a42f02dc217e401eabd
Jakub Matejka <jakub@keboola.com>
2014-04-02 12:24:05 +0000

    test: better naming of sapi logs in tests

--- Tests/Controller/AbstractControllerTest.php
+++ Tests/Controller/AbstractControllerTest.php
@@ -93,7 +93,7 @@ abstract class AbstractControllerTest extends WebTestCase
 		if (preg_match('@\\\\([\w]+)$@', $className, $matches)) {
 			$className = $matches[1];
 		}
-		$logFile = sprintf('%s/sapi-logs/%s-%s.txt', $this->appConfiguration->tmpPath, time(), $className);
+		$logFile = sprintf('%s/sapi-logs/%s-%s.txt', $this->appConfiguration->tmpPath, date('YmdHis'), $className);
 		if (!file_exists($this->appConfiguration->tmpPath . '/sapi-logs')) mkdir($this->appConfiguration->tmpPath . '/sapi-logs');
 		StorageApiClient::setLogger(function($message) use($logFile) {
 			file_put_contents($logFile, $message . PHP_EOL, FILE_APPEND);
