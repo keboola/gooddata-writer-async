64fded872e746a9dffd4db95087cd1a5b62914d6
Jakub Matejka <jakub@keboola.com>
2014-07-29 13:59:00 +0000

    fix: improve ruby error logging

--- GoodData/accept-invitations.rb
+++ GoodData/accept-invitations.rb
@@ -6,9 +6,12 @@ require 'net/http'
 require 'net/imap'
 require 'optparse'
 require 'uri'
+require 'syslog/logger'
 require 'newrelic_rpm'
 
 NewRelic::Agent.manual_start
+
+logger = Syslog::Logger.new 'gooddata-writer'
 
 options = {}
 optparse = OptionParser.new do |opts|
@@ -58,78 +61,87 @@ def fetch(uri_str, limit = 10)
   end
 end
 
-repeat = 0
-last_error = nil
-start_time = Time.now
-begin
+def run
+  repeat = 0
+  last_error = nil
+  start_time = Time.now
   begin
-    imap = Net::IMAP.new options[:host], options[:port], true, nil, false
-  	imap.login options[:email_username], options[:email_password]
+    begin
+      imap = Net::IMAP.new options[:host], options[:port], true, nil, false
+      imap.login options[:email_username], options[:email_password]
 
-  	imap.select 'INBOX'
-    imap.search(['NOT', 'SEEN']).each do |message_id|
-      catch :invitation_ok do
-        envelope = imap.fetch(message_id, 'ENVELOPE')[0].attr['ENVELOPE']
-        if envelope.from[0]['host'] == 'gooddata.com' && envelope.from[0]['mailbox'] == 'invitation'
-          message = imap.fetch(message_id, 'RFC822')[0].attr['RFC822']
-          mail = Mail.read_from_string message
-          mail.body.decoded.split(' ').each { |mail_part|
-            if mail_part =~ URI::regexp && mail_part.start_with?('https://secure.gooddata.com')
-              invitation_id = mail_part.split('/').last
+      imap.select 'INBOX'
+      imap.search(['NOT', 'SEEN']).each do |message_id|
+        catch :invitation_ok do
+          envelope = imap.fetch(message_id, 'ENVELOPE')[0].attr['ENVELOPE']
+          if envelope.from[0]['host'] == 'gooddata.com' && envelope.from[0]['mailbox'] == 'invitation'
+            message = imap.fetch(message_id, 'RFC822')[0].attr['RFC822']
+            mail = Mail.read_from_string message
+            mail.body.decoded.split(' ').each { |mail_part|
+              if mail_part =~ URI::regexp && mail_part.start_with?('https://secure.gooddata.com')
+                invitation_id = mail_part.split('/').last
 
-              GoodData.connect options[:gd_username], options[:gd_password]
-              invitation = GoodData.get '/gdc/account/invitations/' + invitation_id
+                GoodData.connect options[:gd_username], options[:gd_password]
+                invitation = GoodData.get '/gdc/account/invitations/' + invitation_id
 
-              if invitation['invitation']['content']['status'] == 'ACCEPTED'
-                throw :invitation_ok
-              end
+                if invitation['invitation']['content']['status'] == 'ACCEPTED'
+                  throw :invitation_ok
+                end
 
-              result = {
-                  'pid' => invitation['invitation']['links']['project'].split('/').last,
-                  'sender' => invitation['invitation']['meta']['author']['email'],
-                  'createDate' => invitation['invitation']['meta']['created']
-              }
-
-              begin
-                GoodData.post '/gdc/account/invitations/' + invitation_id, {
-                    'invitationStatusAccept' => {
-                        'status' => 'ACCEPTED'
-                    }
+                result = {
+                    'pid' => invitation['invitation']['links']['project'].split('/').last,
+                    'sender' => invitation['invitation']['meta']['author']['email'],
+                    'createDate' => invitation['invitation']['meta']['created']
                 }
-                  result['status'] = 'ok'
-              rescue Exception => e
-                result['status'] = 'error'
-                result['error'] = e.message
+
+                begin
+                  GoodData.post '/gdc/account/invitations/' + invitation_id, {
+                      'invitationStatusAccept' => {
+                          'status' => 'ACCEPTED'
+                      }
+                  }
+                    result['status'] = 'ok'
+                rescue Exception => e
+                  result['status'] = 'error'
+                  result['error'] = e.message
+                end
+
+                puts JSON.generate(result)
+
+                imap.store message_id, '+FLAGS', [:Seen]
               end
-
-              puts JSON.generate(result)
-
-              imap.store message_id, '+FLAGS', [:Seen]
-            end
-          }
+            }
+          end
         end
       end
-    end
-    imap.logout
-    imap.disconnect
-    repeat += 1
-    sleep 10
+      imap.logout
+      imap.disconnect
+      repeat += 1
+      sleep 10
 
-  rescue StandardError, Net::IMAP::IOError => e
-    last_error = e
-    begin
-      imap.disconnect if imap
-    rescue IOError => e
-      #ignore
+    rescue StandardError, Net::IMAP::IOError => e
+      last_error = e
+      begin
+        imap.disconnect if imap
+      rescue IOError => e
+        #ignore
+      end
+      next
     end
-    next
-  end
-end while ((Time.now - start_time) < 300)
+  end while ((Time.now - start_time) < 300)
 
-if repeat == 0
-  if last_error
-    raise last_error
-  else
-    raise 'Could not connect to IMAP without error message'
+  if repeat == 0
+    if last_error
+      raise last_error
+    else
+      raise 'Could not connect to IMAP without error message'
+    end
   end
+end
+
+begin
+  run
+rescue Exception
+  error = { :message => $!.to_s, :trace => $@, :app => 'syrup', :component => 'gooddata-writer', :priority => 'CRITICAL' }
+  logger.fatal error.to_json
 end
