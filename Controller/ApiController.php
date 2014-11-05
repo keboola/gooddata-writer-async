3c46ed74a51f530547219113c149d50319f14776
Jakub Matejka <jakub@keboola.com>
2014-11-05 15:39:39 +0000

    feat: decrease limit of jobs to three days

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -1415,7 +1415,7 @@ class ApiController extends \Syrup\ComponentBundle\Controller\ApiController
 		$this->checkWriterExistence();
 
 		if (empty($this->params['jobId'])) {
-			$days = isset($this->params['days']) ? $this->params['days'] : 7;
+			$days = isset($this->params['days']) ? $this->params['days'] : 3;
 			$tableId = empty($this->params['tableId']) ? null : $this->params['tableId'];
 			$command = empty($this->params['command']) ? null : $this->params['command'];
 			$tokenId = empty($this->params['tokenId']) ? null : $this->params['tokenId'];
